13d076447a0668ab322a5bea8a84b41fa2d30494
fiankasepman <fiankasepman@unknownTheProgrammer.>
2025-12-18 13:33:00 +0700

    init

---
+++ db/sqlc/entry_test.go
@@ -0,0 +1,79 @@
+package db
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomEntry(t *testing.T) Entry {
+	account := CreateRandomAccount(t)
+
+	arg := CreateEntryParams{
+		AccountID: account.ID,
+		Amount:    100,
+	}
+
+	entry, err := testQueries.CreateEntry(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry)
+
+	require.Equal(t, arg.AccountID, entry.AccountID)
+	require.Equal(t, arg.Amount, entry.Amount)
+
+	require.NotZero(t, entry.ID)
+	require.NotZero(t, entry.CreatedAt)
+
+	return entry
+}
+
+func TestCreateEntry(t *testing.T) {
+	createRandomEntry(t)
+}
+
+func TestGetEntry(t *testing.T) {
+	entry1 := createRandomEntry(t)
+
+	entry2, err := testQueries.GetEntry(context.Background(), entry1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry2)
+
+	require.Equal(t, entry1.ID, entry2.ID)
+	require.Equal(t, entry1.AccountID, entry2.AccountID)
+	require.Equal(t, entry1.Amount, entry2.Amount)
+	require.WithinDuration(
+		t,
+		entry1.CreatedAt.Time,
+		entry2.CreatedAt.Time,
+		time.Second,
+	)
+}
+
+func TestListEntries(t *testing.T) {
+	account := CreateRandomAccount(t)
+
+	for i := 0; i < 10; i++ {
+		_, err := testQueries.CreateEntry(context.Background(), CreateEntryParams{
+			AccountID: account.ID,
+			Amount:    int64(i),
+		})
+		require.NoError(t, err)
+	}
+
+	arg := ListEntriesParams{
+		AccountID: account.ID,
+		Limit:     5,
+		Offset:    5,
+	}
+
+	entries, err := testQueries.ListEntries(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, entries, 5)
+
+	for _, entry := range entries {
+		require.NotEmpty(t, entry)
+		require.Equal(t, account.ID, entry.AccountID)
+	}
+}
---
+++ db/sqlc/main_test.go
@@ -0,0 +1,32 @@
+package db
+
+import (
+	"context"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+const (
+	dbSource = "postgresql://postgres:root@localhost:5432/simple_bank?sslmode=disable"
+)
+
+var testQueries *Queries
+var testDB *pgxpool.Pool
+var testStore *Store
+func TestMain(m *testing.M) {
+	var err error
+	testDB, err = pgxpool.New(context.Background(), dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	testQueries = New(testDB)
+	testStore = NewStore(testDB)
+
+	code := m.Run()
+	testDB.Close()
+	os.Exit(code)
+}
