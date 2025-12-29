547011ed2ddb059033e16e680f475fdbba4008f4
fiankasepman <fiankasepman@unknownTheProgrammer.>
2025-12-29 10:04:52 +0700

    make the validator

--- api/server.go
+++ api/server.go
@@ -1,9 +1,13 @@
 package api
 
 import (
-	"github.com/gin-gonic/gin"
 	db "simple-bank/db/sqlc"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 )
+
 // Server serve HTTP requests for our banking service
 type Server struct {
 	store  db.Store
@@ -13,10 +17,17 @@ type Server struct {
 func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
-
+	
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
+	// Account
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccountById)
 	router.GET("/accounts", server.listAccount)
+
+	// Transfer
+	router.POST("/transfers", server.createTransfer)
 
 	server.router = router
 	return server
--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -1,3 +1,12 @@
+CREATE TABLE "users" (
+  "username" varchar PRIMARY KEY,
+  "hashed_password" varchar NOT NULL,
+  "full_name" varchar NOT NULL,
+  "email" varchar [unique] NOT NULL,
+  "password_changed_at" timestamptz [not null] DEFAULT '0001-01-01 00:00:00Z',
+  "created_at" timestamptz NOT NULL DEFAULT now()
+);
+
 CREATE TABLE "accounts" (
   "id" bigserial PRIMARY KEY,
   "owner" varchar NOT NULL,
@@ -21,7 +30,7 @@ CREATE TABLE "transfers" (
   "created_at" timestamptz NOT NULL DEFAULT now()
 );
 
-CREATE INDEX ON "accounts" ("owner");
+CREATE INDEX ON "accounts" ("owner, currency") [unique];
 
 CREATE INDEX ON "entries" ("account_id");
 
@@ -34,6 +43,8 @@ CREATE INDEX ON "transfers" ("from_account_id", "to_account_id");
 COMMENT ON COLUMN "entries"."amount" IS 'can be negative or positive';
 
 COMMENT ON COLUMN "transfers"."amount" IS 'must be positive';
+
+ALTER TABLE "accounts" ADD FOREIGN KEY ("owner") REFERENCES "users" ("username");
 
 ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
 
